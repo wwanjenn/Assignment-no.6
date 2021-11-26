64d42c317028cf0bed57f77be0f3465b59e2a82d
Lemonade-up <wmaglangit1118@gmail.com>
2021-11-26 19:54:53 +0800

    Pseudo Code added

---
+++ Program no.2.py
@@ -0,0 +1,11 @@
+# Program 2: Addition Quiz
+# Create a program that automatically generate two random numbers to add (0 to 99)
+# Let the user answer and evaluate if the user has the correct answer
+# The program will ask the user 10 addition operation
+# Display the result summary of the 10 operations (ex 9/10)
+
+# Steps
+    
+# 1 Generate random num Question, then ask for input
+# 2 Calculate Score
+# 3 Print
