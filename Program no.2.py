e370aa619699e01f8646432c3257ee208e2d5c9c
Lemonade-up <wmaglangit1118@gmail.com>
2021-11-26 20:34:53 +0800

    Fixed score displaying only '1'

--- Program no.2.py
+++ Program no.2.py
@@ -23,57 +23,57 @@ def randomGenerate():
     randomNumber2G = random.randint(0, 99)
     return randomNumber1G, randomNumber2G
 # 2 Generate random number and use it on Function additionQuestion()
-score = 0
+scoreCurrent = 0
 """No.1"""
 randomNumber1, randomNumber2= randomGenerate()
 answerQuestion = additionQuestion('1', randomNumber1, randomNumber2)
-scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, score)
+scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, scoreCurrent)
 
 """No.2"""
 randomNumber1, randomNumber2= randomGenerate()
 answerQuestion = additionQuestion('2', randomNumber1, randomNumber2)
-scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, score)
+scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, scoreCurrent)
 
 """No.3"""
 randomNumber1, randomNumber2= randomGenerate()
 answerQuestion = additionQuestion('3', randomNumber1, randomNumber2)
-scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, score)
+scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, scoreCurrent)
 
 """No.4"""
 randomNumber1, randomNumber2= randomGenerate()
 answerQuestion = additionQuestion('4', randomNumber1, randomNumber2)
-scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, score)
+scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, scoreCurrent)
 
 """No.5"""
 randomNumber1, randomNumber2= randomGenerate()
 answerQuestion = additionQuestion('5', randomNumber1, randomNumber2)
-scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, score)
+scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, scoreCurrent)
 
 """No.6"""
 randomNumber1, randomNumber2= randomGenerate()
 answerQuestion = additionQuestion('6', randomNumber1, randomNumber2)
-scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, score)
+scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, scoreCurrent)
 
 """No.7"""
 randomNumber1, randomNumber2= randomGenerate()
 answerQuestion = additionQuestion('7', randomNumber1, randomNumber2)
-scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, score)
+scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, scoreCurrent)
 
 """No.8"""
 randomNumber1, randomNumber2= randomGenerate()
 answerQuestion = additionQuestion('8', randomNumber1, randomNumber2)
-scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, score)
+scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, scoreCurrent)
 
 """No.9"""
 randomNumber1, randomNumber2= randomGenerate()
 randomNumber1, randomNumber2= randomGenerate()
 answerQuestion = additionQuestion('9', randomNumber1, randomNumber2)
-scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, score)
+scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, scoreCurrent)
 
 """No.10"""
 randomNumber1, randomNumber2= randomGenerate()
 answerQuestion = additionQuestion('10', randomNumber1, randomNumber2)
-scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, score)
+scoreCurrent = scoreTally(answerQuestion, randomNumber1, randomNumber2, scoreCurrent)
 
 # 3 Print
 print(scoreCurrent)
