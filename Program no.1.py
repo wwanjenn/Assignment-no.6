ecbb53503e0596350a8d4a28bc9950578cd03e67
Lemonade-up <wmaglangit1118@gmail.com>
2021-11-26 13:56:56 +0800

    Pseudo Code created

---
+++ Program no.1.py
@@ -0,0 +1,12 @@
+# Program 1: Number Sorter
+# Create a program that ask 4 numbers. 
+# Print the 4 numbers from highest to lowest using only if-else statement.
+
+
+# Steps
+
+# 1 Ask for 4 numbers
+
+
+# 2 If else statement
+# 3 print
