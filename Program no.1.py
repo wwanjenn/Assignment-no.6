47d08e04cd3e12b587d10dd360cfeb05dd3efe36
Lemonade-up <wmaglangit1118@gmail.com>
2021-11-26 14:09:47 +0800

    If-else function for first number complete

--- Program no.1.py
+++ Program no.1.py
@@ -13,5 +13,16 @@ def fourNumbers():
     thirdNumberI = float(input('Third Number: '))
     fourthNumberI = float(input('Fourth Number: '))
     return firstNumberI, secondNumberI, thirdNumberI, fourthNumberI
+
 # 2 If else statement
+def firstNumber(firstIf, secondIf, thirdIf, fourthIf):
+    if firstIf >= secondIf and firstIf >= thirdIf and firstIf >= fourthIf:
+        firstNumberIf = firstIf
+    elif secondIf >= firstIf and secondIf >= thirdIf and secondIf >= fourthIf:
+        firstNumberIf = secondIf
+    elif thirdIf >= firstIf and thirdIf >= secondIf and thirdIf >= fourthIf:
+        firstNumberIf = thirdIf
+    elif fourthIf >= firstIf and fourthIf >= secondIf and fourthIf >= thirdIf:
+        firstNumberIf = fourthIf
+    return firstNumberIf
 # 3 print
